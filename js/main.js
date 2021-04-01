b8dbda708f68e61af3593738487e48d420c1bf9d
Sahan Sarower <sjsahan5@gmail.com>
2021-04-01 15:42:55 +0600

    adding titled animation on picture

--- js/main.js
+++ js/main.js
@@ -24,8 +24,21 @@ $(document).ready(function(){
       $('.accordion .accordion-header span').text('+');
       $(this).children('span').text('-');
     });
-  
-  
-  
+
   });
 
+  function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
